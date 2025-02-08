812da55a23b9897cedf220972e2103e5b5c8350c
Markin Aleksey <alexm97@mail.ru>
2025-02-08 15:51:47 +0300

    add backend

--- providers.tf
+++ providers.tf
@@ -1,10 +1,31 @@
 terraform {
+  required_version = ">1.8.4"
+
+  backend "s3" {
+    
+    region="ru-central1"
+
+    bucket     = "mai-netology-tfstate"
+    key = "production/terraform.tfstate"
+    
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  endpoints ={
+    dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gj5s56d4t25l209u0f/etncr170neo5u60ne267"
+    s3 = "https://storage.yandexcloud.net"
+  }
+
+    dynamodb_table              = "tfstate-lock"
+  }
+
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = ">1.8.4"
 }
 
 provider "yandex" {
